e08025e649f9c6691f7bd0501e0731c21907d0e2
shubhamnevgi <117476919+shubhamnevgi@users.noreply.github.com>
2022-11-06 15:01:14 +0530

    Add files via upload

--- sql6525508.sql
+++ sql6525508.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2022 at 06:30 PM
+-- Generation Time: Nov 06, 2022 at 10:29 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,54 @@ SET time_zone = "+00:00";
 --
 -- Database: `sql6525508`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `Year` varchar(30) NOT NULL,
+  `Branch` varchar(40) NOT NULL,
+  `RegNo` varchar(20) NOT NULL,
+  `SCode` varchar(30) NOT NULL,
+  `Mid` int(20) NOT NULL,
+  `PresentD` varchar(20) NOT NULL,
+  `AbsentD` varchar(20) NOT NULL,
+  `Total` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`Year`, `Branch`, `RegNo`, `SCode`, `Mid`, `PresentD`, `AbsentD`, `Total`) VALUES
+('Second Year (SE)', 'Information Technology', 'T-21-0470', 'ITC301', 11, '15', '2', '13'),
+('Information Technology', 'Second Year (SE)', 'T-21-0470', 'ITC301', 11, '15', '3', '12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iamarks`
+--
+
+CREATE TABLE `iamarks` (
+  `Year` varchar(30) NOT NULL,
+  `Branch` varchar(30) NOT NULL,
+  `RegNo` varchar(30) NOT NULL,
+  `SCode` varchar(30) NOT NULL,
+  `IA` int(11) NOT NULL,
+  `Total` int(11) NOT NULL,
+  `Obtained` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `iamarks`
+--
+
+INSERT INTO `iamarks` (`Year`, `Branch`, `RegNo`, `SCode`, `IA`, `Total`, `Obtained`) VALUES
+('Information Technology', 'Second Year (SE)', 'T-21-0470', 'ITC301', 1, 20, 9);
 
 -- --------------------------------------------------------
 
@@ -44,7 +92,38 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`RegNo`, `Name`, `RollNo`, `Gender`, `Year`, `Branch`, `Address`, `Pass`, `Verified`) VALUES
-('TD-21-4267', 'Shubham', 32, 'Male', 'First Year (FE)', 'Information Technology', '', 'TD-21-4267', 'N');
+('T-21-0470', 'Shubham', 46, 'Male', 'Second Year (SE)', 'Information Technology', '', 'T-21-0470', 'Y'),
+('T-21-0514', 'Sahil ', 23, 'Male', 'Second Year (SE)', 'Information Technology', 'Chiplun', 'T-21-0514', 'N');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `Branch` varchar(30) NOT NULL,
+  `Year` varchar(20) NOT NULL,
+  `SCode` varchar(20) NOT NULL,
+  `SName` varchar(60) NOT NULL,
+  `Sem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`Branch`, `Year`, `SCode`, `SName`, `Sem`) VALUES
+('Information Technology', 'Second Year (SE)', 'ITC301', 'Engineering Mathematics 3', 3),
+('Information Technology', 'Second Year (SE)', 'ITC302', 'Data Structure And Analysis', 3),
+('Information Technology', 'Second Year (SE)', 'ITC303', 'Database Management System', 3),
+('Information Technology', 'Second Year (SE)', 'ITC304', 'Principle of Commmunication', 3),
+('Information Technology', 'Second Year (SE)', 'ITC305', 'Paradigms and Computer Program', 3),
+('Information Technology', 'Second Year (SE)', 'ITC401', 'Engineering Mathematics 4', 4),
+('Information Technology', 'Second Year (SE)', 'ITC402', 'Computer Network and Network Design', 4),
+('Information Technology', 'Second Year (SE)', 'ITC403', 'Operating System', 4),
+('Information Technology', 'Second Year (SE)', 'ITC404', 'Automata Theory', 4),
+('Information Technology', 'Second Year (SE)', 'ITC405', 'Computer Organization and Architecture', 4);
 
 --
 -- Indexes for dumped tables
@@ -55,6 +134,12 @@ INSERT INTO `students` (`RegNo`, `Name`, `RollNo`, `Gender`, `Year`, `Branch`, `
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`RegNo`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`SCode`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
